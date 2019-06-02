5139f1ba186677dd8619ad577553c8309824db93
Mohit Singh <40341375+vicodes@users.noreply.github.com>
2019-06-02 23:12:20 +0530

    Initial Project Structure and files.

---
+++ Sample-Projects/src/main/java/main/SampleProject.java
@@ -0,0 +1,21 @@
+package main;
+
+public class SampleProject {
+	//print  Hello world! in the console
+	public static String printHello() {
+		//print Hello world! in the console!
+		
+		String c = "";
+		c = "Hello World!";
+		return c;
+	}
+	public static int add(int a,int b) {
+		int c = 0;
+		c = a+b;
+		return c;
+	}
+	public static void main(String args[]) {
+		System.out.println(printHello());
+		System.out.println(add(23,45));
+	}
+}
